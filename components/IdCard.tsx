db60179190e1f4cab0f79604b0153a943cad6b99
Mohammed Afrah Usman <162731254+Imafrah@users.noreply.github.com>
2025-10-26 13:26:33 +0530

    Replace logo path with avatar from config

--- components/IdCard.tsx
+++ components/IdCard.tsx
@@ -26,7 +26,7 @@ const IdCard: React.FC = () => {
     };
 
     // Placeholder SVG Logo (Terminal Icon)
-    const logoSvg = 'public/Profile_Headshot.png';
+    const logoSvg = portfolioConfig.avatar;
 
     useEffect(() => {
         const tiltElement = cardRef.current;
